7d78976271c1405d633169ad4933bd4454da814d
eskander-taher <54884978+eskander-taher@users.noreply.github.com>
2025-01-18 17:12:33 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Eskander</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I a software engineer <br className='sm:block hidden' />
+            A software engineer <br className='sm:block hidden' />
              who develops web applications
           </p>
         </div>
